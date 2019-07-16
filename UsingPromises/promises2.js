42d3adea56452c0a1e065bb976d0570090c53b1a
Ruslan Zhukouski <Ruslan_Zhukouski@epam.com>
2019-07-16 10:29:00 +0300

    Using Promises and Ajax

---
+++ UsingPromises/promises2.js
@@ -0,0 +1,37 @@
+'use strict'
+
+let movieList = document.getElementById('movies');
+
+function addMovieToList(movie) {
+    let img = document.createElement('img');
+    img.src = movie.Poster;
+    movieList.appendChild(img);
+}
+
+function getData(url, done) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+               let json = JSON.parse(xhr.response);
+            console.log(json);
+            done(json.Search);
+        } else {
+            console.error(xhr.statusText);
+        }
+    };
+
+    xhr.onerror = function(error) {
+        console.error(error);
+    };
+    
+    xhr.send();
+}
+
+let search = 'spider man';
+
+getData(`http://www.omdbapi.com/?apikey=730993d6&s=${search}`, function(movies) {
+    movies.forEach(function(movie) {
+        addMovieToList(movie);
+    });
+});

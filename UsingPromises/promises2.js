d605ac04413227180ac0da457478130b0c60d971
Ruslan Zhukouski <Ruslan_Zhukouski@epam.com>
2019-07-16 11:30:15 +0300

    Correct promises

--- UsingPromises/promises2.js
+++ UsingPromises/promises2.js
@@ -8,30 +8,38 @@ function addMovieToList(movie) {
     movieList.appendChild(img);
 }
 
-function getData(url, done) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-               let json = JSON.parse(xhr.response);
-            console.log(json);
-            done(json.Search);
-        } else {
-            console.error(xhr.statusText);
-        }
-    };
+function getData(url) {
+    return new Promise(function(resolve, reject){
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                let json = JSON.parse(xhr.response);
+                resolve(json.Search);
+            } else {
+                reject(xhr.statusText);
+            }
+        };
 
-    xhr.onerror = function(error) {
-        console.error(error);
-    };
-    
-    xhr.send();
+        xhr.onerror = function(error) {
+            reject(error);
+        };
+        
+        xhr.send();
+    });
+
 }
-
+    
 let search = 'spider man';
 
-getData(`http://www.omdbapi.com/?apikey=730993d6&s=${search}`, function(movies) {
-    movies.forEach(function(movie) {
-        addMovieToList(movie);
-    });
-});
+getData(`http://www.omdbapi.com/?apikey=730993d6&s=${search}`)
+    .then(movies => movies.forEach (movie => addMovieToList(movie)))
+    .catch(error => console.error(error));
+
+    // Быстрее выполненное обещание
+    // let batman = getData('http://www.omdbapi.com/?apikey=730993d6&s=batman');
+    // let superman = getData('http://www.omdbapi.com/?apikey=730993d6&s=superman');
+
+    // Promise.race([batman, superman])
+    //     .then(movies => movies.forEach (movie => addMovieToList(movie)))
+    //     .catch(error => console.error(error));
--- promises.js
+++ promises.js
@@ -31,3 +31,29 @@ applyForVisa({})
     .then(bookHotel) 
     .then(buyTicket)
     .catch(error => console.error(error));      
+
+
+    /////Выполнение обещания после всех предыдущих
+    function go(num) {
+        return new Promise(function(resolve, reject){
+            let delay = Math.ceil(Math.random() * 3000);
+            console.log(num, delay);
+            setTimeout(() => {
+                if (delay > 2000)
+                    reject(num);
+                else 
+                    resolve(num);
+                }, delay);
+        });
+    }
+
+    let p1 = go(1);
+    let p2 = go(2);
+    let p3 = go(3);
+
+    Promise.all([p2, p1, p3])
+    //Promise.race([p2, p1, p3]) - вывод первого сработавшего обещания
+        .then (value => console.log(value))
+        .catch(error => console.log(error));
+
+
